6823c226c89f2848f501a0b056aaca1f782f843f
Brian Winter <blackpawn8990@gmail.com>
2017-11-11 03:20:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-// Write your solution here!
 const drivers = ["Milo", "Otis", "Garfield" ];
 
 
 
+function destructivelyAppendDriver(){
+
+    drivers.push('Ralph');
+
+;}
+
+function destructivelyPrependDriver(){
+  drivers.unshift('Bob');
+};
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop('Garfield');
+};
+function destructivelyRemoveFirstDriver(){
+  drivers.shift('Milo');
 };
 function appendDriver(){
-    [...drivers, 'Broom'];
-
-
+   const allDrivers = [...drivers, "Broom"];
 
 
 };
