46204d27b02a90361d8b57f3b6980b14ad694759
Brian Winter <blackpawn8990@gmail.com>
2017-11-11 03:25:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,24 +2,8 @@ const drivers = ["Milo", "Otis", "Garfield" ];
 
 
 
-function destructivelyAppendDriver(){
-
-    drivers.push('Ralph');
-
-;}
-
-function destructivelyPrependDriver(){
-  drivers.unshift('Bob');
-};
-
-function destructivelyRemoveLastDriver(){
-  drivers.pop('Garfield');
-};
-function destructivelyRemoveFirstDriver(){
-  drivers.shift('Milo');
-};
 function appendDriver(){
-   const allDrivers = [...drivers, "Broom"];
+   const newArray = [...drivers, "Broom"];
 
 
 };
