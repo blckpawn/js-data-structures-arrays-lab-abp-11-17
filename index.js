2087fb803744775ee2cf99f8aedc0d2597d4f339
Brian Winter <blackpawn8990@gmail.com>
2017-11-11 02:34:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function destructivelyRemoveFirstDriver(){
   drivers.shift('Milo');
 };
 function appendDriver(){
-  const allDrivers = [...drivers, ];
+  const allDrivers = drivers.slice();
 
-  allDrivers.push('Broom');
+
 };
